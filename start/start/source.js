abaa59be1f18c3c7078beda65a13e8608320e5e7
Yasha Mostofi <yasha.mostofi@gmail.com>
2016-08-29 22:29:56 -0500

    added start page

---
+++ start/start/source.js
@@ -0,0 +1,146 @@
+var hoverEffect = true; // set true for hover effect, set false for no hover effect
+
+var searchEngine = 'google'; // default search engine - set google for google search, bing for bing search, yahoo for yahoo search
+
+var numberOfScreens = 2; // set number of screens (1 or 2 or 3)
+ 
+var blockName = new Array(); // set names of blocks
+
+blockName[1] = 'Most used';
+blockName[2] = 'School';
+blockName[3] = 'News & fun';
+
+var bookmark = new Array();
+bookmark[0] = new Array();
+bookmark[1] = new Array();
+bookmark[2] = new Array();
+
+
+// set your bookmarks here: (If you do not fill 'thumb' for thumbnail will be used title)
+// 1. BLOCK
+bookmark[0][0] = {
+'title':'YouTube',
+'url':'http://youtube.com',
+'thumb':'youtube.png'
+};
+bookmark[0][1] = {
+'title':'Weather',
+'url':'http://weather.com/',
+'thumb':'weather.png'
+};
+bookmark[0][2] = {
+'title':'Grooveshark',
+'url':'http://grooveshark.com',
+'thumb':'grooveshark.png'
+};
+bookmark[0][3] = {
+'title':'amazon',
+'url':'http://amazon.com/',
+'thumb':'amazon.png'
+};
+bookmark[0][4] = {
+'title':'twitter',
+'url':'http://twitter.com',
+'thumb':'twitter.png'
+};
+bookmark[0][5] = {
+'title':'google',
+'url':'http://google.com',
+'thumb':'google.png'
+};
+bookmark[0][6] = {
+'title':'facebook',
+'url':'http://facebook.com',
+'thumb':'facebook.png'
+};
+bookmark[0][7] = {
+'title':'BBC news',
+'url':'http://www.bbc.co.uk/news/',
+'thumb':'bbcnews.png'
+};
+bookmark[0][8] = {
+'title':'CNN',
+'url':'http://www.cnn.com',
+'thumb':'cnn.png'
+};
+bookmark[0][9] = {
+'title':'reddit',
+'url':'http://reddit.com/',
+'thumb':'reddit.png'
+};
+bookmark[0][10] = {
+'title':'wikipedia',
+'url':'http://wikipedia.org',
+'thumb':'wikipedia.png'
+};
+bookmark[0][11] = {
+'title':'Dropbox',
+'url':'http://dropbox.com/',
+'thumb':'dropbox.png'
+};
+// end of 1. BLOCK
+
+
+// set your bookmarks here: (If you do not fill 'thumb' for thumbnail will be used title)
+// 2. BLOCK
+bookmark[1][0] = {
+'title':'Canvas',
+'url':'http://naperville.instructure.com/',
+'thumb':'canvas.png'
+};
+bookmark[1][1] = {
+'title':'North Website',
+'url':'http://schools.naperville203.org/north/',
+'thumb':''
+};
+bookmark[1][2] = {
+'title':'Student Email',
+'url':'http://www.outlook.com/',
+'thumb':''
+};
+bookmark[1][3] = {
+'title':'amazon',
+'url':'http://amazon.com/',
+'thumb':'amazon.png'
+};
+bookmark[1][4] = {
+'title':'twitter',
+'url':'http://twitter.com',
+'thumb':'twitter.png'
+};
+bookmark[1][5] = {
+'title':'google',
+'url':'http://google.com',
+'thumb':'google.png'
+};
+bookmark[1][6] = {
+'title':'facebook',
+'url':'http://facebook.com',
+'thumb':'facebook.png'
+};
+bookmark[1][7] = {
+'title':'BBC news',
+'url':'http://www.bbc.co.uk/news/',
+'thumb':'bbcnews.png'
+};
+bookmark[1][8] = {
+'title':'CNN',
+'url':'http://www.cnn.com',
+'thumb':'cnn.png'
+};
+bookmark[1][9] = {
+'title':'Infinite Campus',
+'url':'https://infinitecampus.naperville203.org/campus/portal/naperville.jsp',
+'thumb':''
+};
+bookmark[1][10] = {
+'title':'wikipedia',
+'url':'http://wikipedia.org',
+'thumb':'wikipedia.png'
+};
+bookmark[1][11] = {
+'title':'Dropbox',
+'url':'http://dropbox.com/',
+'thumb':'dropbox.png'
+};
+// end of 2. BLOCK
