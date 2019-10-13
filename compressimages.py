1da22d1cdfdb158ddebfe740c49fa6b04c3c0988
Yasha Mostofi <yasha.mostofi@gmail.com>
2019-10-13 13:32:27 -0700

    Cleanup whitespace

--- compressimages.py
+++ compressimages.py
@@ -6,27 +6,27 @@ from stat import S_IWRITE
 from shutil import move
 from argparse import ArgumentParser
 from abc import ABCMeta, abstractmethod
- 
+
 class ProcessBase:
     """Abstract base class for file processors."""
     __metaclass__ = ABCMeta
- 
+
     def __init__(self):
         self.extensions = []
         self.backupextension = 'compressimages-backup'
- 
+
     @abstractmethod
     def processfile(self, filename):
         """Abstract method which carries out the process on the specified file.
         Returns True if successful, False otherwise."""
         pass
- 
+
     def processdir(self, path):
         """Recursively processes files in the specified directory matching
         the self.extensions list (case-insensitively)."""
- 
+
         filecount = 0 # Number of files successfully updated
- 
+
         for root, dirs, files in walk(path):
             for file in files:
                 # Check file extensions against allowed list
@@ -42,13 +42,13 @@ class ProcessBase:
                     if self.processfile(fullpath):
                         filecount = filecount + 1
         return filecount
- 
+
 class CompressImage(ProcessBase):
     """Processor which attempts to reduce image file size."""
     def __init__(self):
         ProcessBase.__init__(self)
         self.extensions = ['jpg', 'jpeg', 'png']
- 
+
     def processfile(self, filename):
         """Renames the specified image to a backup path,
         and writes out the image again with optimal settings."""
@@ -57,46 +57,46 @@ class CompressImage(ProcessBase):
             if (not stat(filename)[0] & S_IWRITE):
                 print 'Ignoring read-only file "' + filename + '".'
                 return False
-            
+
             # Create a backup
             backupname = filename + '.' + self.backupextension
- 
+
             if isfile(backupname):
                 print 'Ignoring file "' + filename + '" for which existing backup file is present.'
                 return False
- 
+
             rename(filename, backupname)
         except Exception as e:
             stderr.write('Skipping file "' + filename + '" for which backup cannot be made: ' + str(e) + '\n')
             return False
- 
+
         ok = False
- 
+
         try:
             # Open the image
             with open(backupname, 'rb') as file:
                 img = Image.open(file)
- 
+
                 # Check that it's a supported format
                 format = str(img.format)
                 if format != 'PNG' and format != 'JPEG':
                     print 'Ignoring file "' + filename + '" with unsupported format ' + format
                     return False
- 
+
                 # This line avoids problems that can arise saving larger JPEG files with PIL
                 ImageFile.MAXBLOCK = img.size[0] * img.size[1]
-                
+
                 # The 'quality' option is ignored for PNG files
                 img.save(filename, quality=90, optimize=True)
- 
+
             # Check that we've actually made it smaller
             origsize = getsize(backupname)
             newsize = getsize(filename)
- 
+
             if newsize >= origsize:
                 print 'Cannot further compress "' + filename + '".'
                 return False
- 
+
             # Successful compression
             ok = True
         except Exception as e:
@@ -107,16 +107,16 @@ class CompressImage(ProcessBase):
                     move(backupname, filename)
                 except Exception as e:
                     stderr.write('ERROR: could not restore backup file for "' + filename + '": ' + str(e) + '\n')
- 
+
         return ok
- 
+
 class RestoreBackupImage(ProcessBase):
     """Processor which restores image from backup."""
- 
+
     def __init__(self):
         ProcessBase.__init__(self)
         self.extensions = [self.backupextension]
- 
+
     def processfile(self, filename):
         """Moves the backup file back to its original name."""
         try:
@@ -125,14 +125,14 @@ class RestoreBackupImage(ProcessBase):
         except Exception as e:
             stderr.write('Failed to restore backup file "' + filename + '": ' + str(e) + '\n')
             return False
- 
+
 class DeleteBackupImage(ProcessBase):
     """Processor which deletes backup image."""
- 
+
     def __init__(self):
         ProcessBase.__init__(self)
         self.extensions = [self.backupextension]
- 
+
     def processfile(self, filename):
         """Deletes the specified file."""
         try:
@@ -141,7 +141,7 @@ class DeleteBackupImage(ProcessBase):
         except Exception as e:
             stderr.write('Failed to delete backup file "' + filename + '": ' + str(e) + '\n')
             return False
- 
+
 if __name__ == "__main__":
     # Argument parsing
     modecompress = 'compress'
@@ -158,9 +158,9 @@ if __name__ == "__main__":
             + modecompress + ': Compress the image(s). '
             + moderestorebackup + ': Restore the backup images (valid for directory path only). '
             + modedeletebackup + ': Delete the backup images (valid for directory path only).')
- 
+
     args = parser.parse_args()
- 
+
     # Construct processor requested mode
     if args.mode == modecompress:
         processor = CompressImage()
@@ -168,7 +168,7 @@ if __name__ == "__main__":
         processor = RestoreBackupImage()
     elif args.mode == modedeletebackup:
         processor = DeleteBackupImage()
- 
+
     # Run according to whether path is a file or a directory
     if isfile(args.path):
         if args.mode != modecompress:
